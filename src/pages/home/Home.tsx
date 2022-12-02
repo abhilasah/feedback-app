eb46464f2f8d367a7598c5c438ea30c690a5a0c0
Nikhil Sharma <n_sharma@talespin.company>
2022-12-02 18:48:27 +0530

    Feat: landing page added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,13 +7,28 @@ const Home = () => {
                 </div>
                 <div className="cards">
                     <div className="card">
-                        <div className="card-info">
-                            <h2>Spiderman Miles Morales</h2>
-                            <p>PS5 Version</p>
-                            <div className="progress"></div>
+                        <div className="cardContainer">
+                            <div className="card-info">
+                                <h2>Date</h2>
+                            </div>
+                            <h2 className="percentage">01/01/2023</h2>
                         </div>
-                        <h2 className="percentage">60%</h2>
+                        <div className="cardContainer">
+                            <div className="card-info">
+                                <h2>Theme</h2>
+                            </div>
+                            <h2 className="percentage">New Year</h2>
+                        </div>
+                        <div className="cardContainer">
+                            <div className="card-info">
+                                <h2>Venue</h2>
+                            </div>
+                            <h2 className="percentage">4th floor cafeteria</h2>
+                        </div>
                     </div>
+                </div>
+                <div className="pro">
+                    <h2>Let's get started</h2>
                 </div>
             </div>
         </>
