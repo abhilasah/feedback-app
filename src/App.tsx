37b1c1f250ad0026354510bece749bc5829bd48d
avirat1 <a_virat@talespin.company>
2022-12-03 19:59:57 +0530

    Feat: git pages package added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import AppErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import Layout from './components/layout/layout';
 import AppRouter from './router/AppRouter';
 
 function App() {
     console.log(import.meta.env.VITE_APP_TITLE);
-    const [darkMode, setDarkMode] = useState(true);
     return (
         <div>
             <AppErrorBoundary>
