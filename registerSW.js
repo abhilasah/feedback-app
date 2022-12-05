ebb2a5f01f8a5a9ee3844638470020af81e979a6
avirat1 <a_virat@talespin.company>
2022-12-05 11:55:43 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://abhilasah.github.io/feedback-app/sw.js', { scope: 'https://abhilasah.github.io/feedback-app/' })})}
