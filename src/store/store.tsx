2cf5246bd58b95d4fbfa7526005b401925ad938a
Sarang Tayde <sarang.tayde@globant.com>
2022-12-10 23:39:56 +0530

    database config added

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { ref, onValue } from "firebase/database";
+import db from '../database/config';
+
+const dummyReducer = () => {
+    const dbRef = ref(db, '/readme');
+    let data = 'Data ';
+    onValue(
+        dbRef, 
+        snapshot => {
+            data += snapshot.val();
+            console.log(data);
+        },
+        {onlyOnce: true}
+    );
+    return "dummyReducer";
+}
 
 export default configureStore ({
-    reducer:{}
-})
+    reducer:dummyReducer
+});
