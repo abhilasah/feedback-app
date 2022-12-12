bcddd25140b3af7c3d3bfd9a70b946d69856ff26
Nikhil Sharma <n_sharma@talespin.company>
2022-12-12 11:42:44 +0530

    adding localstorage authentication

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,7 @@
 const Layout = ({ children }: { children: any }) => {
     return (
         <>
-            <main>
-                <section className="glass">{children}</section>
-            </main>
-            <div className="circle1"></div>
-            <div className="circle2"></div>
+            <main>{children}</main>
         </>
     );
 };
